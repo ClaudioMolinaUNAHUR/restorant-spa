c3bb42055ba5d757e2931874d1d48a5fcafc3c07
Claudio Molina <molina.claudio@estudiantes.unahur.edu.ar>
2022-09-24 01:03:40 -0300

    terminado pcpal

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const modoBg = document.querySelector("#cambiar-modo");
+const inicio = document.querySelector("#logo");
 
-modoBg.addEventListener('click', cambiarModo);
+eventListeners();
+function eventListeners(){
+    modoBg.addEventListener('click', cambiarModo);
+    inicio.addEventListener('click',volverAInicio);
+}
+
+function  volverAInicio(){
+    document.location.href="index.html";
+}
 
 function cambiarModo(e) { 
     var cuerpoweb = document.body; 
@@ -9,14 +18,14 @@ function cambiarModo(e) {
     if (e.target.classList.contains("btn-light")){
         letrasBlancas.forEach((texto)=> {
             texto.style.color = '#ffb700';
-            texto.classList.remove('text-white')
+            texto.classList.remove('colorprimario')
         });            
         e.target.classList.add("btn-dark");
         e.target.classList.remove("btn-light");
     }else{        
         letrasBlancas.forEach((texto)=> {
             texto.style.color = '#F9F5F0';
-            texto.classList.add('text-white')
+            texto.classList.add('colorprimario')
         });            
         e.target.classList.add("btn-light");
         e.target.classList.remove("btn-dark");
